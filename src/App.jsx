61d1b50731b3347f09aab0bf8cfde19c037169ff
Abián Izquierdo <abizmo@gmail.com>
2022-02-20 22:24:45 +0000

    Add theme configuration

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
+import { ThemeProvider } from 'styled-components';
 import ArticlePreview from './components/ArticlePreview';
 
 import Footer from './components/Footer';
+import theme from './theme';
 
 const App = () => (
-  <>
+  <ThemeProvider theme={theme}>
     <main>
       <ArticlePreview />
     </main>
     <Footer />
-  </>
+  </ThemeProvider>
 );
 
 export default App;
---
+++ src/theme/index.js
@@ -0,0 +1,29 @@
+export default {
+  breakpoint: {
+    xs: '320px',
+    lg: '1110px',
+  },
+  color: {
+    'blue-900': 'hsl(217, 19%, 35%)',
+    'blue-700': 'hsl(214, 17%, 51%)',
+    'blue-500': 'hsl(212, 23%, 69%)',
+    'blue-300': 'hsl(210, 46%, 95%)',
+  },
+  font: {
+    size: {
+      xs: {
+        normal: '0.8125rem',
+        big: '1rem',
+      },
+      lg: {
+        normal: '0.8125rem',
+        big: '1.25rem',
+      },
+    },
+    weight: {
+      regular: 500,
+      bold: 700,
+    },
+  },
+  sizes: {},
+};
